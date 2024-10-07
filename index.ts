c5271e2fc91dae275e83f50a22fc8d20f0d5b9f8
calmis87@gmail.com <calmis87@gmail.com>
2024-10-07 15:18:08 +0100

    refactor cron job scheduling and improve logging functionality

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import cron from "node-cron";
+
 import {
   sendSMS,
   fetchNews,
@@ -9,14 +10,12 @@ import {
 } from "./services/index.ts";
 import { TradingOpportunity } from "./types.ts";
 
-// Array de intervale de timp
 const intervals = [
-  { start: { hour: 14, minute: 30 }, step: 6, count: 10 }, // 10 requesturi la fiecare 6 minute (14:30 - 15:30)
-  { start: { hour: 15, minute: 30 }, step: 15, count: 10 }, // 10 requesturi la fiecare 15 minute (15:30 - 18:00)
-  { start: { hour: 18, minute: 0 }, step: 36, count: 5 }, // 5 requesturi la fiecare 36 minute (18:00 - 21:00)
+  { start: { hour: 14, minute: 30 }, step: 6, count: 10 },
+  { start: { hour: 15, minute: 30 }, step: 15, count: 10 },
+  { start: { hour: 18, minute: 0 }, step: 36, count: 5 },
 ];
 
-// Funcție pentru a programa cron jobs pe baza intervalelor
 function scheduleJobs() {
   let firstTime = true;
 
@@ -24,14 +23,11 @@ function scheduleJobs() {
     let { hour, minute } = interval.start;
     let currentMinute = minute;
 
-    // Planificarea fiecărei execuții
     for (let i = 0; i < interval.count; i++) {
-      // Crearea expresiei cron
       const cronExpression = `${currentMinute} ${hour} * * 1-5`; // Luni-Vineri (1-5)
 
-      // Planificarea job-ului
       cron.schedule(cronExpression, async () => {
-        console.log(`Rulează la ${hour}:${currentMinute}`);
+        logToFile(`Rulează la ${hour}:${currentMinute}`);
 
         if (firstTime) {
           sendSMS("begin jobs");
